2cdcfd2a44ae87bd77f29f6fabe1df6e9ba0b886
1 <grdesu@gmail.com>
2015-03-07 17:31:04 +0600

    Add instagram random photo page

---
+++ js/igp.js
@@ -0,0 +1,32 @@
+$(function(){
+	$( "#getphoto" ).click(function(){
+		client_id = "68ed5b515dc84031b707c351fa227eaf";
+		if (username = $( "#username" ).val())
+			$.ajax({
+				url: "https://api.instagram.com/v1/users/search?q=" + username + "&client_id=68ed5b515dc84031b707c351fa227eaf",
+				dataType: "jsonp",
+				success: function( response ) {
+					userid = response.data[0].id;
+					getrecent_url = "https://api.instagram.com/v1/users/" + userid + "/media/recent/?client_id=" + client_id;
+					$.ajax({
+						url: getrecent_url,
+						dataType: "jsonp",
+						success: function( response ) {
+							console.log(image_url = response.data[Math.round(Math.random()*100)%20].images.standard_resolution.url); // server response
+							$('#img').css('background-image', 'url(' + image_url + ')');
+						}
+					});
+				}
+			});
+		else {
+			$.ajax({
+				url: "https://api.instagram.com/v1/media/popular?client_id=68ed5b515dc84031b707c351fa227eaf",
+				dataType: "jsonp",
+				success: function( response ) {
+					console.log(image_url = response.data[Math.round(Math.random()*100)%20].images.standard_resolution.url); // server response
+					$('#img').css('background-image', 'url(' + image_url + ')');
+				}
+			});
+		};
+	});
+})
